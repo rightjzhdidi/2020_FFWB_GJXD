aa4471b5a2d701ef3e725f4a4c873d70582bcc37
zihao <zihao@users.noreply.github.com>
2021-01-18 14:44:18 +0800

    gjxd_server
    springboot模板

---
+++ gjxd_server/src/main/java/com/wuster/gjxd_server/GjxdServerApplication.java
@@ -0,0 +1,13 @@
+package com.wuster.gjxd_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjxdServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GjxdServerApplication.class, args);
+    }
+
+}
